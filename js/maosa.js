14e17fbd33570856378e818b832ef06e8fa45f0d
Antonio Ortiz Avila <aortiz.muchaweb@gmail.com>
2024-06-18 12:43:02 -0600

    complete tempral information

--- js/maosa.js
+++ js/maosa.js
@@ -15,3 +15,58 @@ window.addEventListener('load', (e) => {
         sendMessagetoWhatsapp()
     })
 });
+
+
+// About
+document.getElementById("maosa-mn-about").addEventListener("click", function() {
+    document.getElementById("main-content-section-about").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("btn-maosa-slider-about").addEventListener("click", function() {
+    document.getElementById("main-content-section-about").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("maosa-mn-about-f").addEventListener("click", function() {
+    document.getElementById("main-content-section-about").scrollIntoView({ behavior: 'smooth' });
+});
+
+// Products
+document.getElementById("maosa-mn-products").addEventListener("click", function() {
+    document.getElementById("main-content-section-products").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("btn-maosa-slider-products").addEventListener("click", function() {
+    document.getElementById("main-content-section-products").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("maosa-mn-products-f").addEventListener("click", function() {
+    document.getElementById("main-content-section-products").scrollIntoView({ behavior: 'smooth' });
+});
+
+// Operation
+document.getElementById("maosa-mn-operation").addEventListener("click", function() {
+    document.getElementById("main-content-section").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("btn-maosa-slider-operation").addEventListener("click", function() {
+    document.getElementById("main-content-section").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("maosa-mn-operation-f").addEventListener("click", function() {
+    document.getElementById("main-content-section").scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// Clients
+document.getElementById("maosa-mn-clients").addEventListener("click", function() {
+    document.getElementById("main-content-section-clients").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("btn-maosa-slider-clients").addEventListener("click", function() {
+    document.getElementById("main-content-section-clients").scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById("maosa-mn-clients-f").addEventListener("click", function() {
+    document.getElementById("main-content-section-clients").scrollIntoView({ behavior: 'smooth' });
+});
+
